5e0bb2e9f972e2c3cd4aabefd7509178562937ad
holimen <support@crazydice.io>
2025-07-07 17:01:54 +0800

    fix: oft

--- contracts/EchoOFT.sol
+++ contracts/EchoOFT.sol
@@ -6,9 +6,8 @@ import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
 contract EchoOFT is OFT, Pausable {
-     // Blacklist
+    // Blacklist
     mapping(address => bool) public isBlackListed;
-    string private __symbol;
 
     event SetBlackList(address account, bool state);
 
@@ -17,9 +16,7 @@ contract EchoOFT is OFT, Pausable {
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {
-        __symbol = _symbol;
-    }
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
 
     function decimals() public pure override returns (uint8) {
         return 8;
@@ -28,17 +25,9 @@ contract EchoOFT is OFT, Pausable {
     function sharedDecimals() public pure override returns (uint8) {
         return 8;
     }
-    
-    function renameSymbol(string memory _symbol) external onlyOwner {
-        __symbol = _symbol;
-    }
-
-    function symbol() public view override returns (string memory) {
-        return __symbol;
-    }
 
     // Blacklist restrict from-address, contains(burn's from-address)
-    function _update(address from, address to, uint256 value) override internal whenNotPaused {
+    function _update(address from, address to, uint256 value) internal override whenNotPaused {
         require(!isBlackListed[from], "from is in blackList");
         super._update(from, to, value);
     }
